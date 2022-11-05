1f3d3a9e84c46c8a11382fcdfe691a69f9562907
little-sealion <tess.zheng@outlook.com>
2022-11-06 08:59:08 +1000

    temp debug

--- RegisterDevices.Test/SetUp/IntegrationTestBase.cs
+++ RegisterDevices.Test/SetUp/IntegrationTestBase.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
-using RegisterDevices.Models;
+﻿//using System;
+//using Microsoft.Extensions.DependencyInjection;
+//using RegisterDevices.Models;
 
-namespace RegisterDevices.Test.SetUp
-{
-    public class IntegrationTestBase:IClassFixture<IntegrationTestFactory<Startup,DeviceContext>>
-    {
-        public readonly IntegrationTestFactory<Startup, DeviceContext> Factory;
+//namespace RegisterDevices.Test.SetUp
+//{
+//    public class IntegrationTestBase:IClassFixture<IntegrationTestFactory<Startup,DeviceContext>>
+//    {
+//        public readonly IntegrationTestFactory<Startup, DeviceContext> Factory;
 
-        public IntegrationTestBase(IntegrationTestFactory<Startup,DeviceContext> factory)
-        {
-            Factory = factory;
-        }
-    }
-}
+//        public IntegrationTestBase(IntegrationTestFactory<Startup,DeviceContext> factory)
+//        {
+//            Factory = factory;
+//        }
+//    }
+//}
 
--- RegisterDevices.Test/SetUp/ServiceCollectionExtensions.cs
+++ RegisterDevices.Test/SetUp/ServiceCollectionExtensions.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿//using System;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.DependencyInjection;
 
-namespace RegisterDevices.Test.SetUp
-{
-    public static class ServiceCollectionExtensions
-    {
-        public static void RemoveDbContext<T>(this IServiceCollection services) where T:DbContext
-        {
-            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<T>));
-            if (descriptor is not null) services.Remove(descriptor);
-        }
+//namespace RegisterDevices.Test.SetUp
+//{
+//    public static class ServiceCollectionExtensions
+//    {
+//        public static void RemoveDbContext<T>(this IServiceCollection services) where T:DbContext
+//        {
+//            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<T>));
+//            if (descriptor is not null) services.Remove(descriptor);
+//        }
 
-        public static void EnsureDbCreated<T>(this IServiceCollection services) where T : DbContext
-        {
-            var serviceProvider = services.BuildServiceProvider();
-            using var scope = serviceProvider.CreateScope();
-            var scopedServices = scope.ServiceProvider;
+//        public static void EnsureDbCreated<T>(this IServiceCollection services) where T : DbContext
+//        {
+//            var serviceProvider = services.BuildServiceProvider();
+//            using var scope = serviceProvider.CreateScope();
+//            var scopedServices = scope.ServiceProvider;
 
-            var context = scopedServices.GetRequiredService<T>();
-            context.Database.EnsureCreated();
-        }
-    }
-}
+//            var context = scopedServices.GetRequiredService<T>();
+//            context.Database.EnsureCreated();
+//        }
+//    }
+//}
 
